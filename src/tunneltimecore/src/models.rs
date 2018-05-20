2c0178add30daa378456b025ac87955b5661667d
Quinten Palmer <quintenpalmer@gmail.com>
2018-05-20 16:14:12 -0400

    Move POST bodies to lower part of models.rs

--- src/tunneltimecore/src/models.rs
+++ src/tunneltimecore/src/models.rs
@@ -1,22 +1,6 @@
 #[derive(Debug, Serialize, Deserialize)]
-pub struct UserID {
-    pub user_id: i32,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct DwarfCreation {
-    pub town_id: i32,
-    pub dwarf_name: String,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
 pub struct User {
     pub id: i32,
-    pub user_name: String,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct NewUser {
     pub user_name: String,
 }
 
@@ -50,4 +34,22 @@ pub enum GemType {
 pub struct Dwarf {
     pub id: i32,
     pub name: String,
+}
+
+// POST bodies
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct UserID {
+    pub user_id: i32,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct DwarfCreation {
+    pub town_id: i32,
+    pub dwarf_name: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct NewUser {
+    pub user_name: String,
 }
